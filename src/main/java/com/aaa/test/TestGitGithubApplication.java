6591f0a96b49455b71d8eda77dbbf403665d8b3c
qy97hexianseng <2446973913@qq.com>
2019-07-25 09:22:28 +0800

    第一次提交到本地仓库，必须填写提交备注，否则不让提交

---
+++ src/main/java/com/aaa/test/TestGitGithubApplication.java
@@ -0,0 +1,13 @@
+package com.aaa.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitGithubApplication.class, args);
+    }
+
+}
