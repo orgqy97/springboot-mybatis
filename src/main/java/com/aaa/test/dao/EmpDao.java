a6046c8580e8792d2a014687cd2dc5412243a7ec
qy97hexianseng <2446973913@qq.com>
2019-07-25 10:03:36 +0800

    新加了dao

---
+++ src/main/java/com/aaa/test/dao/EmpDao.java
@@ -0,0 +1,20 @@
+package com.aaa.test.dao;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * fileName:EmpDao
+ * description:
+ * author:zz
+ * createTime:2019/7/25 10:00
+ * version:1.0.0
+ */
+public interface EmpDao {
+
+    /**
+     *获取员工列表
+     * @return
+     */
+    List<Map>  getListA();
+}
